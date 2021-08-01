865f6c308286c46ee0fffd4cbde266fafaada977
Shirley Leu <shirleyleu@users.noreply.github.com>
2021-08-01 14:13:36 +0200

    Add new output function

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/shirleyleu/xk6-output-timescaledb
 go 1.16
 
 require (
+	github.com/cockroachdb/apd v1.1.0 // indirect
+	github.com/gofrs/uuid v4.0.0+incompatible // indirect
+	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
+	github.com/jackc/pgx v3.6.2+incompatible
+	github.com/lib/pq v1.10.2 // indirect
+	github.com/shopspring/decimal v1.2.0 // indirect
 	go.k6.io/k6 v0.33.0
 	gopkg.in/guregu/null.v3 v3.5.0
 )
--- output.go
+++ output.go
@@ -1,6 +1,9 @@
 package timescaledb
 
 import (
+	"github.com/jackc/pgx"
+	"github.com/sirupsen/logrus"
+
 	"go.k6.io/k6/output"
 	"go.k6.io/k6/stats"
 )
@@ -12,12 +15,43 @@ func init() {
 }
 
 type Output struct {
+	Pool   *pgx.ConnPool
 	Config config
+
+	thresholds map[string][]*dbThreshold
+
+	logger logrus.FieldLogger
 }
 
 func (o *Output) Description() string {
 	return "Output to TimescaleDB"
 }
+
+type dbThreshold struct {
+	id        int
+	threshold *stats.Threshold
+}
+
+// TimescaleDB database schema expected by this collector
+const databaseSchema = `CREATE TABLE IF NOT EXISTS samples (
+		ts timestamptz NOT NULL DEFAULT current_timestamp,
+		metric varchar(128) NOT NULL,
+		tags jsonb,
+		value real
+	);
+	CREATE TABLE IF NOT EXISTS thresholds (
+		id serial,
+		ts timestamptz NOT NULL DEFAULT current_timestamp,
+		metric varchar(128) NOT NULL,
+		tags jsonb,
+		threshold varchar(128) NOT NULL,
+		abort_on_fail boolean DEFAULT FALSE,
+		delay_abort_eval varchar(128),
+		last_failed boolean DEFAULT FALSE
+	);
+	SELECT create_hypertable('samples', 'ts');
+	CREATE INDEX IF NOT EXISTS idx_samples_ts ON samples (ts DESC);
+	CREATE INDEX IF NOT EXISTS idx_thresholds_ts ON thresholds (ts DESC);`
 
 func (o *Output) Start() error {
 	return nil
