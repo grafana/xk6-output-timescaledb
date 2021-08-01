2baf44f6514c975acdaf34803643c8b8f8b85e0f
Shirley Leu <shirleyleu@users.noreply.github.com>
2021-08-01 20:26:45 +0200

    Add periodic flusher and callback

--- output.go
+++ output.go
@@ -1,7 +1,11 @@
 package timescaledb
 
 import (
+	"context"
+	"time"
+
 	"github.com/jackc/pgx"
+	"github.com/jackc/pgx/pgtype"
 	"github.com/sirupsen/logrus"
 
 	"go.k6.io/k6/output"
@@ -14,9 +18,13 @@ func init() {
 	})
 }
 
+var _ output.Output = &Output{}
+
 type Output struct {
-	Pool   *pgx.ConnPool
-	Config config
+	output.SampleBuffer
+	periodicFlusher *output.PeriodicFlusher
+	Pool            *pgx.ConnPool
+	Config          config
 
 	thresholds map[string][]*dbThreshold
 
@@ -77,10 +85,60 @@ func (o *Output) Start() error {
 		}
 	}
 
+	pf, err := output.NewPeriodicFlusher(time.Duration(o.Config.PushInterval.Duration), o.commit)
+	if err != nil {
+		return err
+	}
+
+	o.logger.Debug("TimescaleDB: Running!")
+	o.periodicFlusher = pf
+
 	o.Pool.Release(conn)
 	return nil
 }
 
-func (o *Output) AddMetricSamples(samples []stats.SampleContainer) {}
+func (o *Output) commit() {
+	sampleContainers := o.GetBufferedSamples()
+	for _, sc := range sampleContainers {
+		samples := sc.GetSamples()
+		logrus.Debug("TimescaleDB: Committing...")
+		o.logger.WithField("samples", len(samples)).Debug("TimescaleDB: Writing...")
 
-func (o *Output) Stop() error { return nil }
+		start := time.Now()
+		batch := o.Pool.BeginBatch()
+		for _, s := range samples {
+			tags := s.Tags.CloneTags()
+			batch.Queue("INSERT INTO samples (ts, metric, value, tags) VALUES ($1, $2, $3, $4)",
+				[]interface{}{s.Time, s.Metric.Name, s.Value, tags},
+				[]pgtype.OID{pgtype.TimestamptzOID, pgtype.VarcharOID, pgtype.Float4OID, pgtype.JSONBOID},
+				nil)
+		}
+
+		for _, t := range o.thresholds {
+			for _, threshold := range t {
+				batch.Queue("UPDATE thresholds SET last_failed = $1 WHERE id = $2",
+					[]interface{}{threshold.threshold.LastFailed, threshold.id},
+					[]pgtype.OID{pgtype.BoolOID, pgtype.Int4OID},
+					nil)
+			}
+		}
+
+		if err := batch.Send(context.Background(), nil); err != nil {
+			o.logger.WithError(err).Error("TimescaleDB: Couldn't send batch of inserts")
+		}
+		if err := batch.Close(); err != nil {
+			o.logger.WithError(err).Error("TimescaleDB: Couldn't close batch and release connection")
+		}
+
+		t := time.Since(start)
+		o.logger.WithField("t", t).Debug("TimescaleDB: Batch written!")
+	}
+}
+
+func (o *Output) Stop() error {
+	o.logger.Debug("Stopping...")
+	defer o.logger.Debug("Stopped!")
+	o.periodicFlusher.Stop()
+	o.Pool.Close()
+	return nil
+}
